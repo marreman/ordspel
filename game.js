c00961fca4ded2b7004020dea016934726a65a12
Martin Liberg <martin.liberg@m.co>
2022-08-28 22:34:49 +0200

    Fix the colors

--- game.js
+++ game.js
@@ -1,25 +1,14 @@
-const { log, assert } = console
-
-const test = (actual, expected, message) => {
-  if (JSON.stringify(actual) !== JSON.stringify(expected)) {
-    console.error(message, actual, expected)
-  }
-}
-
-const evaluateGuess = (guess, answer) => {
-  log("evaluateGuess", guess, answer)
-
+export const evaluateGuess = (guess, answer) => {
   const colors = Array(guess.length).fill("gray")
 
-  for (let i = 0; i < colors.length; i++) {
-    if (guess[i] === answer[i]) {
-      colors[i] = "green"
-    }
-  }
-
   for (let i = 0; i < answer.length; i++) {
+    if (answer[i] === guess[i]) {
+      colors[i] = "green"
+      continue
+    }
+
     for (let j = 0; j < guess.length; j++) {
-      if (answer[i] === guess[j] && colors[j] !== "green") {
+      if (answer[i] === guess[j] && i !== j && colors[j] !== "green") {
         colors[j] = "yellow"
         break
       }
@@ -27,61 +16,17 @@ const evaluateGuess = (guess, answer) => {
   }
 
   for (let i = 0; i < colors.length; i++) {
-    if (colors[i] !== "green") continue
-    for (let j = 0; j < guess.length; j++) {
-      if (guess[j] === guess[i] && colors[j] === "gray") {
+    if (colors[i] === "green") {
+      const letter = guess[i]
+      if (count(letter, guess) < count(letter, answer)) {
         colors[i] = "green-yellow"
         break
       }
     }
   }
 
-  // console.log(result)
-  console.log(colors)
-
   return colors
 }
 
-// evaluateGuess("axxxx", "azzzz") // [ "green", "gray", "gray", "gray", "gray" ]
-// evaluateGuess("aaxxx", "aazzz") // [ "green", "green", "gray", "gray", "gray" ]
-// evaluateGuess("aaaxx", "aazzz") // [ "green", "green", "gray", "gray", "gray" ]
-evaluateGuess("axaxx", "aazzz") // [ "green", "gray", "yellow", "gray", "gray" ]
-// evaluateGuess("axaxx", "zzzza") // [ "green", "gray", "yellow", "gray", "gray" ]
-// evaluateGuess("sirap", "paris") // [ "yellow", "yellow", "green", "yellow", "yellow" ]
-evaluateGuess("axxxx", "aazzz") // [ "green-yellow", "gray", "gray", "gray", "gray" ]
-
-// test(
-//   evaluateGuess("axxxx", "azzzz"),
-//   [
-//     ["a", "green"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//   ],
-//   "one for one A, correct place"
-// )
-
-// test(
-//   evaluateGuess("aaxxx", "azzza"),
-//   [
-//     ["a", "green"],
-//     ["a", "gray"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//   ],
-//   "two for two A, one in correct place"
-// )
-
-// test(
-//   evaluateGuess("axxxx", "aazzz"),
-//   [
-//     ["a", "green"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//     ["x", "gray"],
-//   ],
-//   "one for two A, correct place"
-// )
+const count = (letter, word) =>
+  Array.from(word).filter((l) => l === letter).length
